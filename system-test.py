039389a8636f6584c66b0a4a96dfc6c2c3170f22
zlalvani <zlalvani@gmail.com>
2016-11-22 01:33:39 -0500

    added helpers.py, system-test.py

---
+++ system-test.py
@@ -0,0 +1,12 @@
+import helpers
+
+if __name__ == '__main__':
+
+    rpc_client = helpers.RpcClient('predictor-preprocessor')
+
+    with open('test_assets/example.json', 'r') as f:
+        contents = f.read()
+
+    result = rpc_client.call(contents)
+    
+    print(result)
